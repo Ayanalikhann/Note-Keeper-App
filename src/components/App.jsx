c83a1192bb38c74c916c0a3f6e8438b68cdab04a
Ayan Ali Khan <97780902+Ayanalikhann@users.noreply.github.com>
2025-03-28 11:34:51 +0530

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]); // Array to store multiple notes
+
+  function addNote(newNote) {
+    setNotes((prevValue) => [...prevValue, newNote]);
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => prevNotes.filter((note, index) => index !== id));
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea addNote={addNote} />
+      {notes.map((note, index) => (
+        <Note
+          id={index}
+          key={index}
+          title={note.title}
+          content={note.content}
+          deleteNote={deleteNote}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
